de86d4cc19bd292c285712019941353542f33272
adriton <adrtler@gmail.com>
2021-01-09 11:50:28 +0100

    login del usuario

--- controller/auth.js
+++ controller/auth.js
@@ -2,6 +2,7 @@ const { response } = require('express');
 const Usuario = require('../models/usuario');
 const bcrypt = require('bcryptjs');
 const { generarJWT } = require('../helpers/jwt');
+const usuario = require('../models/usuario');
 /**
  * crearUsuario: Realiza el registro del usuario.
  * @param {*} req
@@ -20,20 +21,16 @@ const crearUsuario = async (req, res = response) => {
         msg: 'El correo ya existe',
       });
     }
-
     const usuario = new Usuario(req.body);
-
     // Encriptar contraseña, por defecto 10 vueltas en el salt
     const salt = bcrypt.genSaltSync();
     usuario.password = bcrypt.hashSync(password, salt);
-
     // Guardar usuario en BD
     await usuario.save();
-
     // Generar el JWT
     const token = await generarJWT(usuario._id);
-
     res.json({
+      ok: true,
       usuario,
       token,
     });
@@ -52,13 +49,38 @@ const crearUsuario = async (req, res = response) => {
  * @param {*} res
  */
 const login = async (req, res = response) => {
-  const { email, password } = req.body;
-  res.json({
-    ok: true,
-    msg: 'login',
-    email,
-    password,
-  });
+  try {
+    const { email, password } = req.body;
+    /**Verificar si existe el coreo */
+    const usuarioDB = await Usuario.findOne({ email });
+    if (!usuarioDB) {
+      return res.status(404).json({
+        ok: false,
+        msg: 'Email no encontrado',
+      });
+    }
+    /**Validar el password */
+    const validPassword = bcrypt.compareSync(password, usuarioDB.password);
+    /**Generar el JWT */
+    const token = await generarJWT(usuarioDB._id);
+    if (!validPassword) {
+      return res.status(404).json({
+        ok: false,
+        msg: 'Contraseña incorrecta',
+      });
+    }
+    res.json({
+      ok: true,
+      usuarioDB,
+      token,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: 'Hable con el administrador',
+    });
+  }
 };
 
 /**
