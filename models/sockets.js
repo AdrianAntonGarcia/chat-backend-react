544c64d888603d3ee43b45a910447f9017197c6d
adriton <adrtler@gmail.com>
2021-01-09 12:38:29 +0100

    TODOs de sockets

--- models/sockets.js
+++ models/sockets.js
@@ -1,31 +1,23 @@
-
-
 class Sockets {
+  constructor(io) {
+    this.io = io;
 
-    constructor( io ) {
+    this.socketEvents();
+  }
 
-        this.io = io;
-
-        this.socketEvents();
-    }
-
-    socketEvents() {
-        // On connection
-        this.io.on('connection', ( socket ) => {
-
-            // Escuchar evento: mensaje-to-server
-            socket.on('mensaje-to-server', ( data ) => {
-                console.log( data );
-                
-                this.io.emit('mensaje-from-server', data );
-            });
-            
-        
-        });
-    }
-
-
+  socketEvents() {
+    // On connection
+    this.io.on('connection', (socket) => {
+      // TODO: Validar el JWT
+      // Si el token no es válido, desconectar
+      // TODO: Saber que usuario está activo mediante el UID
+      // TODO: Emitir todos los usuarios conectados
+      // TODO: Socket join, uid
+      // TODO: Escuchar cuándo el cliente manda un mensaje, mensaje-personal
+      // TODO: Manejar el disconnect, marcar en la base de datos que se desconecto
+      // TODO: Emitir todos los usuarios conectados
+    });
+  }
 }
-
 
 module.exports = Sockets;
