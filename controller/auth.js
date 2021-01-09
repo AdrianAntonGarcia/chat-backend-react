05435c8d31bb0fb05270c24eff40084095fb4d84
adriton <adrtler@gmail.com>
2021-01-09 10:55:57 +0100

    Crear usuario en la base de datos

--- controller/auth.js
+++ controller/auth.js
@@ -1,19 +1,38 @@
 const { response } = require('express');
-
+const Usuario = require('../models/usuario');
 /**
  * crearUsuario: Realiza el registro del usuario.
  * @param {*} req
  * @param {*} res
  */
 const crearUsuario = async (req, res = response) => {
-  const { nombre, email, password } = req.body;
-  res.json({
-    ok: true,
-    msg: 'register',
-    nombre,
-    email,
-    password,
-  });
+  try {
+    const { email, password } = req.body;
+    /**
+     * Verificar que el email no exista
+     */
+    const existeEmail = await Usuario.findOne({ email: email });
+    if (existeEmail) {
+      return res.status(400).json({
+        ok: false,
+        msg: 'El correo ya existe',
+      });
+    }
+    // TODO: Encriptar contraseña
+
+    // Guardar usuario en BD
+    const usuario = new Usuario(req.body);
+    await usuario.save();
+    res.json({
+      usuario,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: 'Hable con el administrador',
+    });
+  }
 };
 
 /**
